094f1b8ae6ec49f706b25f998786d335218484c5
nagajayasri08 <165982993+nagajayasri08@users.noreply.github.com>
2025-01-03 20:55:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,27 +5,28 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
+# Define the EC2 instance
 resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-0657605d763ac72a8"  # Use the specified AMI ID
   instance_type = var.instance_type
 
   tags = {
     Name = var.instance_name
   }
+}
+
+# Define other variables
+variable "region" {
+  description = "AWS region"
+  default     = "us-west-1"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  default     = "Provisioned by Terraform"
 }
