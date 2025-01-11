129caaacd586661f5b0305600c60f5d70be00928
nagajayasri08 <165982993+nagajayasri08@users.noreply.github.com>
2025-01-11 18:41:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,15 +5,7 @@
 provider "aws" {
   region ="us-east-1"
 }
-# Define the EC2 instance
-resource "aws_instance" "ubuntu" {
-  ami           = var.ami_id  # Use a variable for the AMI ID
-  instance_type = var.instance_type
 
-  tags = {
-    Name = var.instance_name
-  }
-}
 # Define the S3 bucket
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
