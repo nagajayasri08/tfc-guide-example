adf4b95eb036555f568830aa466266825149dc65
nagajayasri08 <165982993+nagajayasri08@users.noreply.github.com>
2025-01-11 18:44:20 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,13 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-# Output the AMI ID of the EC2 instance
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+# Output the name of the S3 bucket
+output "bucket_name" {
+  value = aws_s3_bucket.example.bucket
 }
 
-# Output the ARN of the EC2 instance
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+# Output the ARN of the S3 bucket
+output "bucket_arn" {
+  value = aws_s3_bucket.example.arn
 }
+
